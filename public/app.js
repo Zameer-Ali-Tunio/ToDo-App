aff951645a2db23250c083595ef62ca36e845958
Zameer Tunio <zameeeralitunio1gmail.com>
2020-07-26 17:54:56 +0500

    ToDO App

---
+++ public/app.js
@@ -0,0 +1,52 @@
+var list=document.getElementById('list')
+function todo(){
+var Todo=document.getElementById('C_Todo')
+    // li
+    if(Todo.value==" "){
+        alert('Nothing To Add')
+    }
+    else{
+var li=document.createElement('li')
+var text=document.createTextNode(Todo.value)
+text.value
+li.appendChild(text)
+// crate btns
+// delte btn
+var dlt=document.createElement('button')
+var dlttext=document.createTextNode('Delete')
+dlt.appendChild(dlttext)
+dlt.setAttribute('onclick','DltBtn(this)')
+dlt.setAttribute('class','rounded btn btn-outline-success ')
+// edit btn
+var edit=document.createElement('button')
+var edittext=document.createTextNode('Edit')
+edit.setAttribute('onclick','editBtn(this)')
+edit.setAttribute('class','rounded btn btn-outline-success')
+console.log(edit)
+
+edit.appendChild(edittext)
+
+li.appendChild(dlt)
+li.appendChild(edit)
+// ul
+var ul=document.getElementById('ul')
+ul.appendChild(li)
+    }
+Todo.value=" "
+}
+
+function dltall(){
+    if(list.innerHTML==""){
+        alert('Nothing to delete')
+    }
+    else{
+        list.innerHTML=""
+    }
+}
+function DltBtn(para){
+para.parentNode.remove()
+}
+function editBtn(para){
+var editval=prompt('Edit ToDo',para.parentNode.firstChild.nodeValue)
+para.parentNode.firstChild.nodeValue=editval
+}
